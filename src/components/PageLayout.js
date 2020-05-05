7a7d55f94c8d4347f17c9f70584fb40a2ccf9281
Pseudocoder Kim <digression99@gmail.com>
2020-05-05 18:10:52 +0900

    Remove comments

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,9 +1,7 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import styled from 'styled-components';
-
-import { GlobalStyle } from "../styles/global"
 import "normalize.css"
+import { GlobalStyle } from "../styles/global"
 import { Footer } from "./Footer"
 
 const Wrapper = styled.div`
@@ -32,41 +30,3 @@ export const PageLayout = ({ children }) => {
     </>
   )
 }
-
-// const Layout = ({ children }) => {
-//   const data = useStaticQuery(graphql`
-//     query SiteTitleQuery {
-//       site {
-//         siteMetadata {
-//           title
-//         }
-//       }
-//     }
-//   `)
-//
-//   return (
-//     <>
-//       <Header siteTitle={data.site.siteMetadata.title} />
-//       <div
-//         style={{
-//           margin: `0 auto`,
-//           maxWidth: 960,
-//           padding: `0 1.0875rem 1.45rem`,
-//         }}
-//       >
-//         <main>{children}</main>
-//         <footer>
-//           © {new Date().getFullYear()}, Built with
-//           {` `}
-//           <a href="https://www.gatsbyjs.org">Gatsby</a>
-//         </footer>
-//       </div>
-//     </>
-//   )
-// }
-//
-
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired,
-// }
-
